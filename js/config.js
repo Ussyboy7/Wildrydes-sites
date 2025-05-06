4a72e9b127cff7a5d463f6cd1540c4894e1d85d7
Uthman Yaro <79841811+Ussyboy7@users.noreply.github.com>
2025-05-06 17:46:25 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_d8Mq03rA8', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3vnu00khjcap8pq963703o4uf8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-2_qD58eH9OU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1l74oircbj0tdjvjeikov7d6c0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
