265c3488be41d3f2f15016256ea892cc0ded6688
Uthman Yaro <79841811+Ussyboy7@users.noreply.github.com>
2025-05-06 16:07:17 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_d8Mq03rA8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3vnu00khjcap8pq963703o4uf8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
